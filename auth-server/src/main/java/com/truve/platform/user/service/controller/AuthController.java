6af93ca01954e08345a93950f50864502ae324e5
김도현 <142032659+l-lyun@users.noreply.github.com>
2026-02-11 20:58:04 +0900

    feat(auth): 카카오 로그인, 로그아웃 적용 (#16)
    * chore: .gitignore env 추가
    
    * chore: 카카오 로그인 필요 환경변수 셋팅
    
    * chore: 카카오 로그인 환경변수명 변경
    
    * refactor: 로그인 관련 폴더 security로 이관
    
    * feat: 카카오 로그인 화면 리다이렉트 구현
    
    * feat: 카카오 로그인 리다이렉트 get 적용
    
    * fix: 카카오 소셜로그인 변수 기본값 주입
    
    * feat: 카카오 로그인 이후 토큰 사용하여 email get
    
    * feat: OAuth 토큰 저장하기 위한 유저 엔티티 구조 변경
    
    * feat: refreshTokenCookieManager 구현
    
    * feat: 카카오 로그인 이메일 기반 jwt 생성
    
    * refactor: 회원가입 시 유저 생성자 변경
    
    * feat: 소셜로그인 테스트용 뷰 제작
    
    * feat: jwt subject 이메일로 변경
    
    * feat: 카카오 로그인 및 reissue로 토큰 반환 구현
    
    * feat: 로그아웃 시 accesstoken 블랙리스트 등록, refreshtoken 쿠키 삭제
    
    * fix: AuthController 프린트문 제거
    
    * docs: reissue, logout 스웨거 세팅
    
    * feat: 카카오 로그인 시 유저 정보 관련 에러 검증 추가

--- auth-server/src/main/java/com/truve/platform/user/service/controller/AuthController.java
+++ auth-server/src/main/java/com/truve/platform/user/service/controller/AuthController.java
@@ -1,14 +1,19 @@
 package com.truve.platform.user.service.controller;
 
 import org.springframework.data.util.Pair;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.truve.platform.common.response.ApiResult;
 import com.truve.platform.user.service.domain.dto.request.AuthRequest;
 import com.truve.platform.user.service.domain.dto.response.AuthResponse;
+import com.truve.platform.user.service.security.AuthCookieManager;
 import com.truve.platform.user.service.service.AuthService;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -16,6 +21,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -24,6 +30,7 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/auth")
 public class AuthController {
 	private final AuthService authService;
+	private final AuthCookieManager cookieManager;
 
 	@PostMapping("/sign-up")
 	public ApiResult<Void> signUp(
@@ -53,5 +60,58 @@ public class AuthController {
 		AuthResponse.Login res = new AuthResponse.Login(tokens.getFirst(), tokens.getSecond());
 		return ApiResult.ok(res);
 	}
+
+	@Operation(summary = "토큰 재발급")
+	@ApiResponses({
+		@ApiResponse(
+			responseCode = "200",
+			description = "Access Token 재발급 성공",
+			content = @Content(
+				schema = @Schema(implementation = AuthResponse.Login.class)
+			)
+		)
+	})
+	@PostMapping("/reissue")
+	public ApiResult<AuthResponse.Login> reissue(
+		@CookieValue(name = "refreshToken") String refreshToken,
+		HttpServletResponse response
+	) {
+		Pair<String, String> tokens = authService.reissue(refreshToken);
+
+		String newAccessToken = tokens.getFirst();
+		String newRefreshToken = tokens.getSecond();
+
+		cookieManager.setRefreshToken(
+			response,
+			newRefreshToken,
+			60L * 60 * 24 * 14
+		);
+
+		// TODO: 자체 로그인 리프레시 토큰 전달 방식 변경 시 DTO 같이 변경
+		return ApiResult.ok(new AuthResponse.Login(newAccessToken, null));
+	}
+
+
+	@Operation(summary = "로그아웃")
+	@ApiResponses({
+		@ApiResponse(
+			responseCode = "200",
+			description = "로그아웃 성공"
+		),
+	})
+	@DeleteMapping("/logout")
+	public ApiResult<Void> logout(
+		@RequestHeader("X-User-Id") String userId,
+		@RequestHeader("X-Token") String accessToken,
+		HttpServletResponse response
+	) {
+
+		authService.logout(Long.parseLong(userId), accessToken);
+
+		cookieManager.clearRefreshToken(response);
+
+		return ApiResult.ok();
+	}
+
 
 }
