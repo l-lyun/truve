553eee3d050f88123a7952a996c8e09375cdc93c
김도현 <142032659+l-lyun@users.noreply.github.com>
2026-02-13 19:57:17 +0900

    fix(auth): 자체 로그인 응답 방식 변경 (#19)
    * feat: 자체 로그인 시 refreshToken setcookie
    
    * fix: controller 내 변수명 명확히 변경
    
    * docs: auth-server 스웨거 명시적으로 추가

--- auth-server/src/main/java/com/truve/platform/user/service/controller/AuthController.java
+++ auth-server/src/main/java/com/truve/platform/user/service/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.truve.platform.user.service.controller;
 
 import org.springframework.data.util.Pair;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -27,15 +29,15 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
+@Tag(name = "Auth", description = "인증/인가 API")
 public class AuthController {
 	private final AuthService authService;
-	private final AuthCookieManager cookieManager;
+	private final AuthCookieManager authCookieManager;
 
 	@PostMapping("/sign-up")
 	public ApiResult<Void> signUp(
 		@RequestBody  @Valid AuthRequest.SignUp request
 	) {
-		System.out.println(request.getEmail());
 		authService.signUp(request.getEmail(), request.getPassword());
 
 		return ApiResult.ok();
@@ -51,13 +53,25 @@ public class AuthController {
 		),
 	})
 	@PostMapping("/login")
-	public ApiResult<AuthResponse.Login> login(
+	public ResponseEntity<AuthResponse.Login> login(
+		HttpServletResponse httpServletResponse,
 		@RequestBody @Valid AuthRequest.Login request
 	) {
 		Pair<String,String> tokens = authService.login(request.getEmail(), request.getPassword());
 
-		AuthResponse.Login res = new AuthResponse.Login(tokens.getFirst(), tokens.getSecond());
-		return ApiResult.ok(res);
+		String accessToken = tokens.getFirst();
+		String refreshToken = tokens.getSecond();
+
+		var response = new AuthResponse.Login(accessToken);
+
+
+		authCookieManager.setRefreshToken(
+			httpServletResponse,
+			refreshToken,
+			60L * 60 * 24 * 14
+			);
+
+		return ResponseEntity.ok(response);
 	}
 
 	@Operation(summary = "토큰 재발급")
@@ -73,21 +87,22 @@ public class AuthController {
 	@PostMapping("/reissue")
 	public ApiResult<AuthResponse.Login> reissue(
 		@CookieValue(name = "refreshToken") String refreshToken,
-		HttpServletResponse response
+		HttpServletResponse httpServletResponse
 	) {
 		Pair<String, String> tokens = authService.reissue(refreshToken);
 
 		String newAccessToken = tokens.getFirst();
 		String newRefreshToken = tokens.getSecond();
 
-		cookieManager.setRefreshToken(
-			response,
+		authCookieManager.setRefreshToken(
+			httpServletResponse,
 			newRefreshToken,
 			60L * 60 * 24 * 14
 		);
 
-		// TODO: 자체 로그인 리프레시 토큰 전달 방식 변경 시 DTO 같이 변경
-		return ApiResult.ok(new AuthResponse.Login(newAccessToken, null));
+		var response = new AuthResponse.Login(newAccessToken);
+
+		return ApiResult.ok(response);
 	}
 
 
@@ -102,12 +117,12 @@ public class AuthController {
 	public ApiResult<Void> logout(
 		@RequestHeader("X-User-Id") String userId,
 		@RequestHeader("X-Token") String accessToken,
-		HttpServletResponse response
+		HttpServletResponse httpServletResponse
 	) {
 
 		authService.logout(Long.parseLong(userId), accessToken);
 
-		cookieManager.clearRefreshToken(response);
+		authCookieManager.clearRefreshToken(httpServletResponse);
 
 		return ApiResult.ok();
 	}
