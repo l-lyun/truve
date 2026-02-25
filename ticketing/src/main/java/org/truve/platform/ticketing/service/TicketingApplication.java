55fdbf03b1213f9fed021f18679d9da8f57a8004
김도현 <142032659+l-lyun@users.noreply.github.com>
2026-02-25 18:46:00 +0900

    feat(ticketing): 티켓팅 서비스 레디스 메서드 및 jwt 파싱 로직 구현 (#27)
    * feat: ticketing server initial commit
    
    # Conflicts:
    #	ticketing/build.gradle
    
    * feat: 티켓팅 서버 환경 세팅
    
    * feat: ticketing 서비스 RedisSupport 구현
    
    * feat: ticketing-service jwt 로직 추가
    
    * refactor: RedisSupport 누락 메서드 추가
    
    * refactor: RedisSupport 내 메서드 반환형 primitive로 변경
    
    * fix: 미사용 베이스 패키지 삭제

--- ticketing/src/main/java/org/truve/platform/ticketing/service/TicketingApplication.java
+++ ticketing/src/main/java/org/truve/platform/ticketing/service/TicketingApplication.java
@@ -2,7 +2,9 @@ package org.truve.platform.ticketing.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication(scanBasePackages = "com.truve.platform")
 public class TicketingApplication {
 
