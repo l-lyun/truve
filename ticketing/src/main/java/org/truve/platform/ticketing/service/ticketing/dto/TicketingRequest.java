9ddb2e72a371869110188c421bd7679b3094d6e7
김도현 <142032659+l-lyun@users.noreply.github.com>
2026-08-25 11:52:41 +0900

    fix(ticketing): 좌석 수 초과 오류 코드를 유지

--- ticketing/src/main/java/org/truve/platform/ticketing/service/ticketing/dto/TicketingRequest.java
+++ ticketing/src/main/java/org/truve/platform/ticketing/service/ticketing/dto/TicketingRequest.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -14,7 +13,6 @@ public class TicketingRequest {
 	@AllArgsConstructor
 	public static class HoldSeat {
 		@NotEmpty
-		@Size(max = 4)
 		List<@NotNull Long> scheduledSeatIds;
 	}
 
@@ -22,7 +20,6 @@ public class TicketingRequest {
 	@AllArgsConstructor
 	public static class DeleteHoldSeat {
 		@NotEmpty
-		@Size(max = 4)
 		List<@NotNull Long> scheduledSeatIds;
 	}
 }
